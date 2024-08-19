81f9211c080cf83d7c2081a973aa87920c0f764d
Ouali Mustapha Dev-Mus <haithamkanuaa3@gmail.com>
2024-08-19 17:56:01 +0100

    feat: add a button to redirect between pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,27 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const config: Record<
   "home" | "about" | "notFound",
-  { title: string; description?: string }
+  {
+    title: string;
+    description?: string;
+    redirectTo?: { link: string; description: string };
+  }
 > = {
   home: {
     title: "How to Deploy a Vite React App using Nginx server",
     description:
       "Learn the step-by-step process of deploying your Vite React application on an Nginx server. This comprehensive guide covers everything from building your project to configuring Nginx for optimal performance and security. Whether you're a seasoned developer or just getting started, you'll find practical tips and clear instructions to get your app live and running smoothly on the web.",
+    redirectTo: { link: "/about", description: "Go to About Us" },
   },
   about: {
     title: "About Us",
     description: `Welcome to the About Us page of our Vite React app! Here, you'll learn more about our mission, values, and the passionate team behind this project. We're dedicated to creating innovative, high-performance web applications that enhance user experiences and drive success. Our journey began with a simple goal: to leverage cutting-edge technology to solve real-world problems. Over time, we've grown into a dynamic team of developers, designers, and tech enthusiasts, all committed to delivering exceptional digital solutions. Explore this page to discover our story, meet our team, and understand what drives us forward. We're excited to share our vision with you!`,
+    redirectTo: { link: "/home", description: "Go to Home" },
   },
   notFound: {
     title: "Oops! Page Not Found",
     description: `Sorry, the page you're looking for doesn't exist or has been moved. But don't worry, we're here to help you get back on track!`,
+    redirectTo: { link: "/home", description: "Go to Home" },
   },
 };
 
--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,14 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Section({
   title,
   description = "",
+  redirectTo,
 }: {
   title: string;
   description?: string;
+  redirectTo?: {
+    link: string;
+    description: string;
+  };
 }) {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate(redirectTo?.link ?? "/");
+  };
+
   return (
     <>
       <h1>{title}</h1>
       <p className="read-the-docs">{description}</p>
+      {redirectTo?.description && redirectTo?.link && (
+        <button className="navbar-button" onClick={handleButtonClick}>
+          {redirectTo?.description}
+        </button>
+      )}
     </>
   );
 }
